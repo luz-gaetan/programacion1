bdbf5ec47962a6fecbfaa506aaf4fbc99bdd44b1
vaalenclemente <166743677+vaalenclemente@users.noreply.github.com>
2025-07-07 19:44:31 -0300

    Update menu_multinivel.py

--- menu_multinivel.py
+++ menu_multinivel.py
@@ -5,7 +5,7 @@ Fecha: 27/05/2025
 Autor: Grupo 7 (Nicolas Mendez, Valentina Clemente)
 ----------------------------------------------------------------------------------------------------
 Título: Sistema de Gestión Inmobiliaria
-Autores: Grupo 7 - Joaquina Dias, María Luz Gaetan, Nicolas Mendez, Valentina Clemente, Brendan Russell
+Autores: Grupo 7 - Nicolas Mendez, Valentina Clemente, (Brendan Russell solo valido el mail) el resto de los integrantes no hizo absolutamente nada.
 Fecha: 27/05/2025
 Materia: Programación I
 
